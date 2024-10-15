ad512d90aef00f68669401fc0a8bed2298779236
Graham Percival <gperciva@tarsnap.com>
2024-10-14 20:53:51 -0700

    Ignore usr.bin/clang, and reorganize file

--- generate-man-problem-lists.sh
+++ generate-man-problem-lists.sh
@@ -8,26 +8,33 @@
 #   sys/cddl/
 #   sys/contrib/
 #   sys/crypto/
-# In addition to that, ntp has an upstream.
 #
-# We also ignore /tests/ directories, plus 04.csh/ and cxgbetool.8 because
-# they have an unusual amount of errors that aren't yet fixed.
+# The next "paragraph" are files and directories that are not documented as
+# having an upstream, but which either definitely or probably have one.
+#
+# We also ignore /tests/ directories.
+#
+# Finally, we ignore a few extra files and directories because they have an
+# unusual amount of errors that aren't yet fixed.
 if [ ! -e man-pages.txt ]; then
-	git ls-files "*.[1-9]" --			\
-		":(exclude)contrib/"			\
-		":(exclude)crypto/"			\
-		":(exclude)sys/cddl/"			\
-		":(exclude)sys/contrib/"		\
-		":(exclude)sys/crypto/"			\
-							\
-		":(exclude)bin/sh/tests/"		\
-		":(exclude)share/doc/usd/04.csh/"	\
-		":(exclude)share/man/man4/iwlwififw.4"	\
-		":(exclude)usr.bin/bmake/tests/"	\
-		":(exclude)usr.bin/sed/tests/"		\
+	git ls-files "*.[1-9]" --				\
+		":(exclude)contrib/"				\
+		":(exclude)crypto/"				\
+		":(exclude)sys/cddl/"				\
+		":(exclude)sys/contrib/"			\
+		":(exclude)sys/crypto/"				\
+								\
+		":(exclude)usr.bin/clang/"			\
+		":(exclude)usr.sbin/ntp/doc/ntp*"		\
+		":(exclude)usr.sbin/ntp/doc/sntp.8"		\
+								\
+		":(exclude)bin/sh/tests/"			\
+		":(exclude)usr.bin/bmake/tests/"		\
+		":(exclude)usr.bin/sed/tests/"			\
+								\
+		":(exclude)share/doc/usd/04.csh/"		\
+		":(exclude)share/man/man4/iwlwififw.4"		\
 		":(exclude)usr.sbin/cxgbetool/cxgbetool.8"	\
-		":(exclude)usr.sbin/ntp/doc/ntp*"	\
-		":(exclude)usr.sbin/ntp/doc/sntp.8"	\
 		> man-pages.txt
 fi
 
