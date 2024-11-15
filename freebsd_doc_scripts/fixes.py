c52a30f07c2868a58b0e0baf154ac0ea4dcea4cd
Graham Percival <gperciva@tarsnap.com>
2024-11-14 20:15:28 -0800

    Fix: new sentence, new line (license only)
    This fixes problems in FreeBSD's lib/libgssapi/.  Not useful for anything else.

--- freebsd_doc_scripts/fixes.py
+++ freebsd_doc_scripts/fixes.py
@@ -1,5 +1,7 @@
 """ Functions to fix issues in man pages. """
 
+import textwrap
+
 import freebsd_doc_scripts.man_lines
 
 
@@ -60,6 +62,29 @@ def fix_backslashes(man, _args, _mlo=None):
             man.replace_line(i, newline)
 
 
+def new_sentence_legal(man, _args, _mlo=None):
+    """ Fix 'new sentence, new line' warning in license. """
+    section = man.lines.three_way_split(
+        lambda line: "derivative works.  However," in line,
+        lambda line: line.startswith(".Pp"))
+    if not section:
+        return
+
+    # Find the space after the period, then split the line.
+    index = section[0].index(".") + 1
+    firstline = section[0]
+    section.insert(1, firstline[index:].lstrip())
+    section[0] = firstline[:index].rstrip()
+
+    # Rewrap at column 69, but keep the first line unchanged.
+    # The previous line (before this section) ends at column 69, so it must be
+    # at least that width; but if we rewrap it at column 70 (after commenting
+    # out the modified lines 0 and 1), then it changes the text.
+    newlines = [section[0]] + textwrap.wrap("\n".join(section[1:]), width=69)
+
+    man.lines.replace_middle(newlines)
+
+
 FIXES = {
     "unusual Xr order": sort_seealso,
     "skipping paragraph macro: PP after SS": pp_after,
@@ -67,4 +92,5 @@ FIXES = {
 
     # This isn't a mandoc lint message.
     "_backslashes": fix_backslashes,
+    "license new sentence, new line": new_sentence_legal,
 }
